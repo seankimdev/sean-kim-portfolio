d0fa3ac61e84ca566a8c13f335d8cf463bfa0d4b
Sean Kim <seankim0423@gmail.com>
2021-08-11 00:03:29 -0400

    drawer menu added

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,31 @@
 // };
 
 const menuBtn = document.querySelector(".menu-btn");
+const drawer = document.querySelector(".drawer-container");
+const navBar = document.querySelector(".nav-bar");
 let menuOpen = false;
+
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
+    drawer.classList.add("open-drawer");
+    navBar.classList.add("nav-bar-black");
     menuOpen = true;
   } else {
     menuBtn.classList.remove("open");
+    drawer.classList.remove("open-drawer");
+    navBar.classList.remove("nav-bar-black");
     menuOpen = false;
   }
+});
+
+const drawerList = document.querySelectorAll(".drawer li");
+
+drawerList.forEach((list) => {
+  list.addEventListener("click", () => {
+    menuBtn.classList.remove("open");
+    drawer.classList.remove("open-drawer");
+    navBar.classList.remove("nav-bar-black");
+    menuOpen = false;
+  });
 });
